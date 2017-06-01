24ce7e6891985f8a0b0005b5d7fcba65eaa40f11
Sho-n3 <nkc.shota.tanikawa@gmail.com>
2017-06-01 13:50:23 +0900

    no message

---
+++ Grasses100Persent/Assets/Scripts/Main.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Main : MonoBehaviour {
+
+    private bool ShotF;//角度変更中か否か判定フラグ
+
+	void Update () {
+
+        //ロングタップ開始時角度変更を止める
+        if (Input.GetButtonDown("Fire1")){
+            ShotF = !ShotF;
+        }
+
+        if (ShotF){
+            Debug.Log("発射するセリフの種類を決めています。");
+        }
+        else{
+            Debug.Log("セリフを発射する角度を変えています。");
+        }
+	}
+}
